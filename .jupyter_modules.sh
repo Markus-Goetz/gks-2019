dada20bb5db0964f427df61aea168d04dfe15fdc
Markus Goetz <markus.goetz@kit.edu>
2018-08-29 08:34:47 +0200

    Slides and deployment scripts

---
+++ .jupyter_modules.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+module load python/3.6.1
+module load keras/2.1.3
+module load mpi4py/3.0.0-openmpi_2.1.2
+module load matplotlib/2.1.0
+module load tensorflow/1.7.0-gcc_5.4.0-cuda_9.1.85
--- deploy.py
+++ deploy.py
@@ -15,28 +15,44 @@
 
 
 import os
-import pwd
+# import pwd
 import subprocess as sp
 
 
-users = ['padced17']
+# users = ['padced17']
+users = ["train{:03d}".format(i) for i in range(23, 53)]
+# users = ["train{:03d}".format(i) for i in range(23, 24)]
 modules = [
     "python/3.6.1",
     "keras/2.1.3",
     "mpi4py/3.0.0-openmpi_2.1.2",
     "matplotlib/2.1.0",
-    "tensorflow/1.7.0-gcc_5.4.0-cude_9.1.85"]
+    "tensorflow/1.7.0-gcc_5.4.0-cuda_9.1.85"]
+filename = '.jupyter_modules.sh'
+filecontent = ""
+filecontent += "#!/usr/bin/env bash\n"
+for m in modules:
+    filecontent += ("module load " + m + "\n")
+with open(filename, 'w') as f:
+    f.write(filecontent)
 
 for user in users:
-    home = "/gpfs/homea/padcedu/" + user
-    pwnam = pwd.getpwnam(user)
-    os.chdir(pwnam.pw_dir)
-    sp.call(['git', 'clone', 'git://github.com/markus-goetz/gridka-2018.git'])
-    os.chown('gridka-2018', pwnam.pw_uid, pwnam.pw_gid)
+    # home = "/gpfs/homea/padcedu/" + user
+    # pwnam = pwd.getpwnam(user)
+    # os.chdir(pwnam.pw_dir)
+    # sp.call(['git', 'clone', 'git://github.com/markus-goetz/gridka-2018.git'])
+    # os.chown('gridka-2018', pwnam.pw_uid, pwnam.pw_gid)
 
-    modulescript = '.jupyter_modules.sh'
-    with open(modulescript, 'w') as f:
-        f.write("#!/usr/bin/env bash")
-        for m in modules:
-            f.write("module load " + m)
-    os.chown(modulescript, pwnam.pw_uid, pwnam.pw_gid)
+    # get the repo
+    print(user)
+    host = user + "@juron.fz-juelich.de"
+    # command = "git clone git://github.com/markus-goetz/gridka-2018.git"
+    # sp.call(["ssh", host, command])
+
+
+    # write the setup script
+
+    # command = "echo " + filecontent + " > " + filename
+    # sp.call(["ssh", host, command])
+    sp.call(["scp", filename, host+':'])
+    # os.chown(modulescript, pwnam.pw_uid, pwnam.pw_gid)
